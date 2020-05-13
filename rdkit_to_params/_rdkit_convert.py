f7852c57e7cb1cda471c3011e0d89f79190034f1
matteoferla <matteo.ferla@gmail.com>
2020-05-13 15:22:48 +0100

    :beetle: and this is why you don't test in production

--- rdkit_to_params/_rdkit_convert.py
+++ rdkit_to_params/_rdkit_convert.py
@@ -66,7 +66,7 @@ class _RDKitCovertMixin(_RDKitPrepMixin):
         if isinstance(atomnames, dict):
             for k, v in atomnames.items():
                 self._set_PDBInfo_atomname(mol.GetAtomWithIdx(k), v, overwrite=True)
-            self.polish_mol(name)
+            self.polish_mol()
         self.convert_mol()
         return self
 
